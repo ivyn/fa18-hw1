8660ecb9202bad6afaf27daf0aa4ac435397be25
Ivy Nguyen <ivyn@berkeley.edu>
2018-09-20 12:33:14 -0700

    Completed hw1

--- foobar.rb
+++ foobar.rb
@@ -1,8 +1,9 @@
 class Foobar
 
   def self.baz(a)
-    # Class method, not an instance method
-    # Call with `Foobar.baz`
-    # Q4 CODE HERE
+    b = a.map {|a| (a.to_i + 2)}
+    c = b.reject {|b| b % 2 == 1 || b >= 10}
+    d = c.uniq
+    e = d.inject(:+)
   end
 end
